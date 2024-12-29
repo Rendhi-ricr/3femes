49bac8045a70b7ea0f52940cc0281c294a758cde
rendhi-ricr <rendhirichardo2@gmail.com>
2024-12-29 23:28:42 +0700

    dashboard admin

---
+++ app/Views/layouts/back/sidebar.php
@@ -0,0 +1,48 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand text-center" href="index.html">
+            <span class="align-center">3 FEMMES</span>
+        </a>
+        <ul class="sidebar-nav">
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Produk</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Komentar</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Pesanan</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Pembayaran</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Pengiriman</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="<?= base_url('admin') ?>">
+                    <i class="feather" data-feather="trending-up"></i> <span class="trending-up">Laporan</span>
+                </a>
+            </li>
+        </ul>
+
+
+
+    </div>
+</nav>
